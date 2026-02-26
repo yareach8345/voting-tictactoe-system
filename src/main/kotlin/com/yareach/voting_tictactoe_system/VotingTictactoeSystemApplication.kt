be7f82c26cf57d009e82345035b6c100a30b43e8
yareach8345 <mine8345@naver.com>
2026-02-26 16:59:59 +0900

    feature - project initialized

---
+++ src/main/kotlin/com/yareach/voting_tictactoe_system/VotingTictactoeSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.yareach.voting_tictactoe_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VotingTictactoeSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<VotingTictactoeSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/yareach/voting_tictactoe_system/VotingTictactoeSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yareach.voting_tictactoe_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VotingTictactoeSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
