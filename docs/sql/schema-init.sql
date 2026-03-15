6ecc5e0a3b02e929b7493de5f06f0efb60ab0fb5
yareach8345 <mine8345@naver.com>
2026-03-15 11:32:42 +0900

    fix - schema-init.sql 修正
    問題 - テストの途中、playerテーブルのgroup_idカラムがuniqueで設定されているのを発見。
    解決 - unique制約を除去しました。

--- docs/sql/schema-init.sql
+++ docs/sql/schema-init.sql
@@ -21,7 +21,7 @@ create table if not exists game_group_info(
  */
 create table if not exists player(
     id int primary key auto_increment,
-    group_id varchar(255) not null unique,
+    group_id varchar(255) not null,
     user_id varchar(255) not null,
     team enum('O', 'X') default null,
 
